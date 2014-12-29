d1b68b796add61eb66ce9388d9601dac3785c377
= <=>
2014-12-29 21:42:21 +0800

    Exercise 12

---
+++ Chapter1/12.scm
@@ -0,0 +1,15 @@
+#lang planet neil/sicp
+
+(define (Pascal n row)
+  (cond ((< row n) 0)
+       ((= row n) 1)
+       ((= n 1) 1)
+       (else (+ 
+              (Pascal (dec n) (dec row)) 
+              (Pascal n (dec row))))))
+
+(Pascal 1 5)
+(Pascal 2 5)
+(Pascal 3 5)
+(Pascal 4 5)
+(Pascal 5 5)
